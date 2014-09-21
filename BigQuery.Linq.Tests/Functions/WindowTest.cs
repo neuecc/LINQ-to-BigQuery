4604eaf72e824101411fb7bf6eb1c2f83a612553
neuecc <ils@neue.cc>
2014-09-21 21:39:09 +0900

    Complete Window Function

---
+++ BigQuery.Linq.Tests/Functions/WindowTest.cs
@@ -0,0 +1,437 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigQuery.Linq.Tests.Functions
+{
+    [TestClass]
+    public class WindowTest
+    {
+        [TableName("[publicdata:samples.shakespeare]")]
+        class Shakespeare
+        {
+            public string word { get; set; }
+            public string corpus { get; set; }
+            public int word_count { get; set; }
+        }
+
+        [TestMethod]
+        public void CUME_DIST()
+        {
+            var context = new BigQuery.Linq.BigQueryContext();
+
+            var query1 = context.From<Shakespeare>()
+                .Where(x => x.corpus == "othello" && BqFunc.Length(x.word) > 10)
+                .Select(x => new
+                {
+                    x.word,
+                    x.word_count,
+                    cume_dist = BqFunc.CumulativeDistribution(x)
+                        .PartitionBy(y => y.corpus)
+                        .OrderByDescending(y => y.word_count)
+                })
+                .Limit(5)
+                .ToString();
+
+            query1.Is(@"
+SELECT
+  [word],
+  [word_count],
+  CUME_DIST() OVER (PARTITION BY [corpus] ORDER BY [word_count] DESC) AS [cume_dist]
+FROM
+  [publicdata:samples.shakespeare]
+WHERE
+  (([corpus] = 'othello') AND (LENGTH([word]) > 10))
+LIMIT 5
+".TrimSmart());
+        }
+
+        [TestMethod]
+        public void DenseRANK()
+        {
+            var context = new BigQuery.Linq.BigQueryContext();
+
+            var query1 = context.From<Shakespeare>()
+                .Where(x => x.corpus == "othello" && BqFunc.Length(x.word) > 10)
+                .Select(x => new
+                {
+                    x.word,
+                    x.word_count,
+                    dense_rank = BqFunc.DenseRank(x)
+                        .PartitionBy(y => y.corpus)
+                        .OrderByDescending(y => y.word_count)
+                })
+                .Limit(5)
+                .ToString();
+
+            query1.Is(@"
+SELECT
+  [word],
+  [word_count],
+  DENSE_RANK() OVER (PARTITION BY [corpus] ORDER BY [word_count] DESC) AS [dense_rank]
+FROM
+  [publicdata:samples.shakespeare]
+WHERE
+  (([corpus] = 'othello') AND (LENGTH([word]) > 10))
+LIMIT 5
+".TrimSmart());
+        }
+
+        [TestMethod]
+        public void Lag()
+        {
+            var context = new BigQuery.Linq.BigQueryContext();
+
+            var query1 = context.From<Shakespeare>()
+                .Where(x => x.corpus == "othello" && BqFunc.Length(x.word) > 10)
+                .Select(x => new
+                {
+                    x.word,
+                    x.word_count,
+                    lag = BqFunc.Lag(x, y => y.word, 1)
+                        .PartitionBy(y => y.corpus)
+                        .OrderByDescending(y => y.word_count)
+                })
+                .Limit(5)
+                .ToString();
+
+            query1.Is(@"
+SELECT
+  [word],
+  [word_count],
+  LAG([word], 1) OVER (PARTITION BY [corpus] ORDER BY [word_count] DESC) AS [lag]
+FROM
+  [publicdata:samples.shakespeare]
+WHERE
+  (([corpus] = 'othello') AND (LENGTH([word]) > 10))
+LIMIT 5
+".TrimSmart());
+
+            var query2 = context.From<Shakespeare>()
+                .Where(x => x.corpus == "othello" && BqFunc.Length(x.word) > 10)
+                .Select(x => new
+                {
+                    x.word,
+                    x.word_count,
+                    lag = BqFunc.Lag(x, y => y.word, 1, "defv")
+                        .PartitionBy(y => y.corpus)
+                        .OrderByDescending(y => y.word_count)
+                })
+                .Limit(5)
+                .ToString();
+
+            query2.Is(@"
+SELECT
+  [word],
+  [word_count],
+  LAG([word], 1, 'defv') OVER (PARTITION BY [corpus] ORDER BY [word_count] DESC) AS [lag]
+FROM
+  [publicdata:samples.shakespeare]
+WHERE
+  (([corpus] = 'othello') AND (LENGTH([word]) > 10))
+LIMIT 5
+".TrimSmart());
+        }
+
+        [TestMethod]
+        public void Lead()
+        {
+            var context = new BigQuery.Linq.BigQueryContext();
+
+            var query1 = context.From<Shakespeare>()
+                .Where(x => x.corpus == "othello" && BqFunc.Length(x.word) > 10)
+                .Select(x => new
+                {
+                    x.word,
+                    x.word_count,
+                    lead = BqFunc.Lead(x, y => y.word, 1)
+                        .PartitionBy(y => y.corpus)
+                        .OrderByDescending(y => y.word_count)
+                })
+                .Limit(5)
+                .ToString();
+
+            query1.Is(@"
+SELECT
+  [word],
+  [word_count],
+  LEAD([word], 1) OVER (PARTITION BY [corpus] ORDER BY [word_count] DESC) AS [lead]
+FROM
+  [publicdata:samples.shakespeare]
+WHERE
+  (([corpus] = 'othello') AND (LENGTH([word]) > 10))
+LIMIT 5
+".TrimSmart());
+
+            var query2 = context.From<Shakespeare>()
+                .Where(x => x.corpus == "othello" && BqFunc.Length(x.word) > 10)
+                .Select(x => new
+                {
+                    x.word,
+                    x.word_count,
+                    lead = BqFunc.Lead(x, y => y.word, 1, "defv")
+                        .PartitionBy(y => y.corpus)
+                        .OrderByDescending(y => y.word_count)
+                })
+                .Limit(5)
+                .ToString();
+
+            query2.Is(@"
+SELECT
+  [word],
+  [word_count],
+  LEAD([word], 1, 'defv') OVER (PARTITION BY [corpus] ORDER BY [word_count] DESC) AS [lead]
+FROM
+  [publicdata:samples.shakespeare]
+WHERE
+  (([corpus] = 'othello') AND (LENGTH([word]) > 10))
+LIMIT 5
+".TrimSmart());
+        }
+
+        [TestMethod]
+        public void NthValue()
+        {
+            var context = new BigQuery.Linq.BigQueryContext();
+
+            var query1 = context.From<Shakespeare>()
+                .Where(x => x.corpus == "othello" && BqFunc.Length(x.word) > 10)
+                .Select(x => new
+                {
+                    x.word,
+                    x.word_count,
+                    nth = BqFunc.NthValue(x, y => y.word, 1)
+                        .PartitionBy(y => y.corpus)
+                        .OrderByDescending(y => y.word_count)
+                })
+                .Limit(5)
+                .ToString();
+
+            query1.Is(@"
+SELECT
+  [word],
+  [word_count],
+  NTH_VALUE([word], 1) OVER (PARTITION BY [corpus] ORDER BY [word_count] DESC) AS [nth]
+FROM
+  [publicdata:samples.shakespeare]
+WHERE
+  (([corpus] = 'othello') AND (LENGTH([word]) > 10))
+LIMIT 5
+".TrimSmart());
+        }
+
+        [TestMethod]
+        public void Ntile()
+        {
+            var context = new BigQuery.Linq.BigQueryContext();
+
+            var query1 = context.From<Shakespeare>()
+                .Where(x => x.corpus == "othello" && BqFunc.Length(x.word) > 10)
+                .Select(x => new
+                {
+                    x.word,
+                    x.word_count,
+                    ntile = BqFunc.Ntile(x, 2)
+                        .PartitionBy(y => y.corpus)
+                        .OrderByDescending(y => y.word_count)
+                })
+                .Limit(5)
+                .ToString();
+
+            query1.Is(@"
+SELECT
+  [word],
+  [word_count],
+  NTILE(2) OVER (PARTITION BY [corpus] ORDER BY [word_count] DESC) AS [ntile]
+FROM
+  [publicdata:samples.shakespeare]
+WHERE
+  (([corpus] = 'othello') AND (LENGTH([word]) > 10))
+LIMIT 5
+".TrimSmart());
+        }
+
+        [TestMethod]
+        public void PercentRank()
+        {
+            var context = new BigQuery.Linq.BigQueryContext();
+
+            var query1 = context.From<Shakespeare>()
+                .Where(x => x.corpus == "othello" && BqFunc.Length(x.word) > 10)
+                .Select(x => new
+                {
+                    x.word,
+                    x.word_count,
+                    prank = BqFunc.PercentRank(x)
+                        .PartitionBy(y => y.corpus)
+                        .OrderByDescending(y => y.word_count)
+                })
+                .Limit(5)
+                .ToString();
+
+            query1.Is(@"
+SELECT
+  [word],
+  [word_count],
+  PERCENT_RANK() OVER (PARTITION BY [corpus] ORDER BY [word_count] DESC) AS [prank]
+FROM
+  [publicdata:samples.shakespeare]
+WHERE
+  (([corpus] = 'othello') AND (LENGTH([word]) > 10))
+LIMIT 5
+".TrimSmart());
+        }
+
+        [TestMethod]
+        public void Percentile()
+        {
+            var context = new BigQuery.Linq.BigQueryContext();
+
+            var query1 = context.From<Shakespeare>()
+                .Where(x => x.corpus == "othello" && BqFunc.Length(x.word) > 10)
+                .Select(x => new
+                {
+                    x.word,
+                    x.word_count,
+                    p_cont = BqFunc.PercentileCont(x, 0.5)
+                        .PartitionBy(y => y.corpus)
+                        .OrderByDescending(y => y.word_count)
+                })
+                .Limit(5)
+                .ToString();
+
+            query1.Is(@"
+SELECT
+  [word],
+  [word_count],
+  PERCENTILE_CONT(0.5) OVER (PARTITION BY [corpus] ORDER BY [word_count] DESC) AS [p_cont]
+FROM
+  [publicdata:samples.shakespeare]
+WHERE
+  (([corpus] = 'othello') AND (LENGTH([word]) > 10))
+LIMIT 5
+".TrimSmart());
+
+            var query2 = context.From<Shakespeare>()
+                .Where(x => x.corpus == "othello" && BqFunc.Length(x.word) > 10)
+                .Select(x => new
+                {
+                    x.word,
+                    x.word_count,
+                    p_disc = BqFunc.PercentileDisc(x, 0.5)
+                        .PartitionBy(y => y.corpus)
+                        .OrderByDescending(y => y.word_count)
+                })
+                .Limit(5)
+                .ToString();
+
+            query2.Is(@"
+SELECT
+  [word],
+  [word_count],
+  PERCENTILE_DISC(0.5) OVER (PARTITION BY [corpus] ORDER BY [word_count] DESC) AS [p_disc]
+FROM
+  [publicdata:samples.shakespeare]
+WHERE
+  (([corpus] = 'othello') AND (LENGTH([word]) > 10))
+LIMIT 5
+".TrimSmart());
+        }
+
+        [TestMethod]
+        public void Rank()
+        {
+            var context = new BigQuery.Linq.BigQueryContext();
+
+            var query1 = context.From<Shakespeare>()
+                .Where(x => x.corpus == "othello" && BqFunc.Length(x.word) > 10)
+                .Select(x => new
+                {
+                    x.word,
+                    x.word_count,
+                    rank = BqFunc.Rank(x)
+                        .PartitionBy(y => y.corpus)
+                        .OrderByDescending(y => y.word_count)
+                })
+                .Limit(5)
+                .ToString();
+
+            query1.Is(@"
+SELECT
+  [word],
+  [word_count],
+  RANK() OVER (PARTITION BY [corpus] ORDER BY [word_count] DESC) AS [rank]
+FROM
+  [publicdata:samples.shakespeare]
+WHERE
+  (([corpus] = 'othello') AND (LENGTH([word]) > 10))
+LIMIT 5
+".TrimSmart());
+        }
+
+        [TestMethod]
+        public void RatioToReport()
+        {
+            var context = new BigQuery.Linq.BigQueryContext();
+
+            var query1 = context.From<Shakespeare>()
+                .Where(x => x.corpus == "othello" && BqFunc.Length(x.word) > 10)
+                .Select(x => new
+                {
+                    x.word,
+                    x.word_count,
+                    r_to_r = BqFunc.RatioToReport(x, y => y.word_count)
+                        .PartitionBy(y => y.corpus)
+                        .OrderByDescending(y => y.word_count)
+                })
+                .Limit(5)
+                .ToString();
+
+            query1.Is(@"
+SELECT
+  [word],
+  [word_count],
+  RATIO_TO_REPORT([word_count]) OVER (PARTITION BY [corpus] ORDER BY [word_count] DESC) AS [r_to_r]
+FROM
+  [publicdata:samples.shakespeare]
+WHERE
+  (([corpus] = 'othello') AND (LENGTH([word]) > 10))
+LIMIT 5
+".TrimSmart());
+        }
+
+        [TestMethod]
+        public void Rownumber()
+        {
+            var context = new BigQuery.Linq.BigQueryContext();
+
+            var query1 = context.From<Shakespeare>()
+                .Where(x => x.corpus == "othello" && BqFunc.Length(x.word) > 10)
+                .Select(x => new
+                {
+                    x.word,
+                    x.word_count,
+                    row_num = BqFunc.RowNumber(x)
+                        .PartitionBy(y => y.corpus)
+                        .OrderByDescending(y => y.word_count)
+                })
+                .Limit(5)
+                .ToString();
+
+            query1.Is(@"
+SELECT
+  [word],
+  [word_count],
+  ROW_NUMBER() OVER (PARTITION BY [corpus] ORDER BY [word_count] DESC) AS [row_num]
+FROM
+  [publicdata:samples.shakespeare]
+WHERE
+  (([corpus] = 'othello') AND (LENGTH([word]) > 10))
+LIMIT 5
+".TrimSmart());
+        }
+    }
+}
--- BigQuery.Linq/Functions/BqFunc.Window.cs
+++ BigQuery.Linq/Functions/BqFunc.Window.cs
@@ -9,12 +9,36 @@ namespace BigQuery.Linq
 {
     public static partial class BqFunc
     {
+        // https://developers.google.com/bigquery/query-reference#windowfunctions
+
+        // Window functions enable calculations on a specific partition, or "window", of a result set. Each window function expects an OVER clause that specifies the partition, in the following syntax:
+        // OVER ([PARTITION BY <expr>] [ORDER BY <expr>])
+
+        /// <summary>
+        /// Returns a double that indicates the cumulative distribution of a value in a group of values, calculated using the formula &lt;number of rows preceding or tied with the current row&gt; / &lt;total rows&gt;.
+        /// <para>Tied values return the same cumulative distribution value.</para>
+        /// <para>This window function requires ORDER BY in the OVER clause.</para>
+        /// </summary>
         [WindowFunction]
         public static FirstWindowFunction<T> CumulativeDistribution<T>(T fieldSource)
         {
             return new FirstWindowFunction<T>("CUME_DIST", "");
         }
 
+        /// <summary>
+        /// Returns the integer rank of a value in a group of values. The rank is calculated based on comparisons with other values in the group.
+        /// <para>Tied values display as the same rank. The rank of the next value is incremented by 1. For example, if two values tie for rank 2, the next ranked value is 3. If you prefer a gap in the ranking list, use rank().</para>
+        /// <para>This window function requires ORDER BY in the OVER clause.</para>
+        /// </summary>
+        [WindowFunction]
+        public static FirstWindowFunction<T> DenseRank<T>(T fieldSource)
+        {
+            return new FirstWindowFunction<T>("DENSE_RANK", "");
+        }
+
+        /// <summary>
+        /// Returns the value of [expr] for the row located [offset] rows before the current row. If the row doesn't exist, [default_value] returns.
+        /// </summary>
         [WindowFunction]
         public static FirstWindowFunction<T> Lag<T, TK>(T fieldSource, Expression<Func<T, TK>> expr, int offset)
         {
@@ -22,11 +46,106 @@ namespace BigQuery.Linq
             return new FirstWindowFunction<T>("LAG", arg1 + ", " + offset);
         }
 
+        /// <summary>
+        /// Returns the value of [expr] for the row located [offset] rows before the current row. If the row doesn't exist, [default_value] returns.
+        /// </summary>
         [WindowFunction]
         public static FirstWindowFunction<T> Lag<T, TK>(T fieldSource, Expression<Func<T, TK>> expr, int offset, TK defaultValue)
         {
             var arg1 = BigQueryTranslateVisitor.BuildQuery(0, 0, expr);
             return new FirstWindowFunction<T>("LAG", arg1 + ", " + offset + ", " + DataTypeFormatter.Format(defaultValue));
+        }
+
+        /// <summary>
+        /// Returns the value of [expr] for the row located [offset] rows after the current row. If the row doesn't exist, [default_value] returns.
+        /// </summary>
+        [WindowFunction]
+        public static FirstWindowFunction<T> Lead<T, TK>(T fieldSource, Expression<Func<T, TK>> expr, int offset)
+        {
+            var arg1 = BigQueryTranslateVisitor.BuildQuery(0, 0, expr);
+            return new FirstWindowFunction<T>("LEAD", arg1 + ", " + offset);
+        }
+
+        /// <summary>
+        /// Returns the value of [expr] for the row located [offset] rows after the current row. If the row doesn't exist, [default_value] returns.
+        /// </summary>
+        [WindowFunction]
+        public static FirstWindowFunction<T> Lead<T, TK>(T fieldSource, Expression<Func<T, TK>> expr, int offset, TK defaultValue)
+        {
+            var arg1 = BigQueryTranslateVisitor.BuildQuery(0, 0, expr);
+            return new FirstWindowFunction<T>("LEAD", arg1 + ", " + offset + ", " + DataTypeFormatter.Format(defaultValue));
+        }
+
+        /// <summary>
+        /// Returns the value of [expr] at position [n] of the window frame, where [n] is a one-based index.
+        /// </summary>
+        [WindowFunction]
+        public static FirstWindowFunction<T> NthValue<T, TK>(T fieldSource, Expression<Func<T, TK>> expr, int n)
+        {
+            var arg1 = BigQueryTranslateVisitor.BuildQuery(0, 0, expr);
+            return new FirstWindowFunction<T>("NTH_VALUE", arg1 + ", " + n);
+        }
+
+        /// <summary>
+        /// Divides a sequence of rows into [num_buckets] buckets and assigns a corresponding bucket number, as an integer, with each row. The ntile() function assigns the bucket numbers as equally as possible and returns a value from 1 to [num_buckets] for each row.
+        /// </summary>
+        [WindowFunction]
+        public static FirstWindowFunction<T> Ntile<T>(T fieldSource, int numBuckets)
+        {
+            return new FirstWindowFunction<T>("NTILE", numBuckets.ToString());
+        }
+
+        /// <summary>
+        /// Returns the rank of the current row, relative to the other rows in the partition. Returned values range between 0 and 1, inclusively. The first value returned is 0.0.
+        /// <para>This window function requires ORDER BY in the OVER clause.</para>
+        /// </summary>
+        [WindowFunction]
+        public static FirstWindowFunction<T> PercentRank<T>(T fieldSource)
+        {
+            return new FirstWindowFunction<T>("PERCENT_RANK", "");
+        }
+
+        /// <summary>
+        /// Returns values that are based upon linear interpolation between the values of the group, after ordering them per the ORDER BY clause.
+        /// <para>[percentile] must be between 0 and 1.</para>
+        /// <para>This window function requires ORDER BY in the OVER clause.</para>
+        /// </summary>
+        [WindowFunction]
+        public static FirstWindowFunction<T> PercentileCont<T>(T fieldSource, double percentile)
+        {
+            return new FirstWindowFunction<T>("PERCENTILE_CONT", percentile.ToString());
+        }
+
+        /// <summary>
+        /// Returns the value with the smallest cumulative distribution that's greater or equal to [percentile].
+        /// <para>[percentile] must be between 0 and 1.</para>
+        /// <para>This window function requires ORDER BY in the OVER clause.</para>
+        /// </summary>
+        [WindowFunction]
+        public static FirstWindowFunction<T> PercentileDisc<T>(T fieldSource, double percentile)
+        {
+            return new FirstWindowFunction<T>("PERCENTILE_DISC", percentile.ToString());
+        }
+
+        /// <summary>
+        /// <para>Returns the integer rank of a value in a group of values. The rank is calculated based on comparisons with other values in the group.</para>
+        /// <para>Tied values display as the same rank. The rank of the next value is incremented according to how many tied values occurred before it. For example, if two values tie for rank 2, the next ranked value is 4, not 3. If you prefer no gaps in the ranking list, use dense_rank().</para>
+        /// <para>This window function requires ORDER BY in the OVER clause.</para>
+        /// </summary>
+        [WindowFunction]
+        public static FirstWindowFunction<T> Rank<T>(T fieldSource)
+        {
+            return new FirstWindowFunction<T>("RANK", "");
+        }
+
+        /// <summary>
+        /// Returns the ratio of each value to the sum of the values, as a double between 0 and 1.
+        /// </summary>
+        [WindowFunction]
+        public static FirstWindowFunction<T> RatioToReport<T, TK>(T fieldSource, Expression<Func<T, TK>> columnSelector)
+        {
+            var arg1 = BigQueryTranslateVisitor.BuildQuery(0, 0, columnSelector);
+            return new FirstWindowFunction<T>("RATIO_TO_REPORT", arg1);
         }
 
         /// <summary>
