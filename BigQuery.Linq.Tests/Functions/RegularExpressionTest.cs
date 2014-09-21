9239645f42ba2b7ac9deef1adcccad19a6b3c660
neuecc <ils@neue.cc>
2014-09-21 10:23:02 +0900

    Complete Regexp

--- BigQuery.Linq.Tests/Functions/RegularExpressionTest.cs
+++ BigQuery.Linq.Tests/Functions/RegularExpressionTest.cs
@@ -10,15 +10,66 @@ namespace BigQuery.Linq.Tests.Functions
     [TestClass]
     public class RegularExpressionTest
     {
+        [TableName("publicdata:samples.shakespeare")]
+        class Shakespeare
+        {
+            public string word { get; set; }
+        }
+
         public BigQueryContext Ctx { get { return new BigQueryContext(); } }
 
         [TestMethod]
         public void RegexpMatch()
         {
-            var word = "";
-            Ctx.Select<bool>(() => BqFunc.RegexpMatch(word, @"\w\w\'\w\w"))
-                .ToFlatSql()
-                .Is(@"SELECT REGEXP_MATCH([word], r'\w\w\'\w\w')");
+            Ctx.From<Shakespeare>()
+                .Where(x => BqFunc.RegexpMatch(x.word, @"\w\w\'\w\w"))
+                .Select(x => new { x.word, count = BqFunc.Count(x.word) })
+                .GroupBy(x => x.word)
+                .OrderByDescending(x => x.count)
+                .Limit(3)
+                .ToString()
+                .Is(@"
+SELECT
+  [word],
+  COUNT([word]) AS [count]
+FROM
+  [publicdata:samples.shakespeare]
+WHERE
+  REGEXP_MATCH([word], r'\w\w\'\w\w')
+GROUP BY
+  [word]
+ORDER BY
+  [count] DESC
+LIMIT 3
+".TrimSmart());
+        }
+
+        [TestMethod]
+        public void RegexpExtract()
+        {
+            Ctx.From<Shakespeare>()
+                .Select(x => new { fragment = BqFunc.RegexpExtract(x.word, @"(\w\w\'\w\w)") })
+                .GroupBy(x => x.fragment)
+                .OrderBy(x => x.fragment)
+                .Limit(3)
+                .ToString()
+                .Is(@"
+SELECT
+  REGEXP_EXTRACT([word], r'(\w\w\'\w\w)') AS [fragment]
+FROM
+  [publicdata:samples.shakespeare]
+GROUP BY
+  [fragment]
+ORDER BY
+  [fragment]
+LIMIT 3
+".TrimSmart());
+        }
+
+        [TestMethod]
+        public void Replace()
+        {
+            Ctx.Select(() => BqFunc.RegexpReplace("Hello", "lo", "p")).ToFlatSql().Is(@"SELECT REGEXP_REPLACE('Hello', r'lo', 'p')");
         }
     }
 }
--- BigQuery.Linq/Functions/BqFunc.RegularExpression.cs
+++ BigQuery.Linq/Functions/BqFunc.RegularExpression.cs
@@ -9,8 +9,33 @@ namespace BigQuery.Linq
 {
     public static partial class BqFunc
     {
+        // BigQuery provides regular expression support using the re2 library; see that documentation for its regular expression syntax.
+        // Note that the regular expressions are global matches; to start matching at the beginning of a word you must use the ^ character.
+
+        /// <summary>
+        /// Returns true if str matches the regular expression. For string matching without regular expressions, use CONTAINS instead of REGEXP_MATCH.
+        /// </summary>
         [FunctionName("REGEXP_MATCH", SpecifiedFormatterType = typeof(RegexpMatchFormatter))]
         public static bool RegexpMatch(string str, string regexp)
+        {
+            throw Invalid();
+        }
+
+        /// <summary>
+        /// Returns the portion of str that matches the capturing group within the regular expression.
+        /// </summary>
+        [FunctionName("REGEXP_EXTRACT", SpecifiedFormatterType = typeof(RegexpExtractFormatter))]
+        public static bool RegexpExtract(string str, string regexp)
+        {
+            throw Invalid();
+        }
+
+        /// <summary>
+        /// Returns a string where any substring of orig_str that matches reg_exp is replaced with replace_str.
+        /// <para>For example, REGEXP_REPLACE ('Hello', 'lo', 'p') returns Help.</para>
+        /// </summary>
+        [FunctionName("REGEXP_REPLACE", SpecifiedFormatterType = typeof(RegexpReplaceFormatter))]
+        public static bool RegexpReplace(string originalStr, string regexp, string replaceStr)
         {
             throw Invalid();
         }
@@ -25,6 +50,37 @@ namespace BigQuery.Linq
                 var expr2 = innnerTranslator.VisitAndClearBuffer(node.Arguments[1]);
 
                 var format = string.Format("REGEXP_MATCH({0}, r{1})", expr1, expr2);
+
+                return format;
+            }
+        }
+
+        class RegexpExtractFormatter : ISpeficiedFormatter
+        {
+            public string Format(System.Linq.Expressions.MethodCallExpression node)
+            {
+                var innnerTranslator = new BigQueryTranslateVisitor(0, 0);
+
+                var expr1 = innnerTranslator.VisitAndClearBuffer(node.Arguments[0]);
+                var expr2 = innnerTranslator.VisitAndClearBuffer(node.Arguments[1]);
+
+                var format = string.Format("REGEXP_EXTRACT({0}, r{1})", expr1, expr2);
+
+                return format;
+            }
+        }
+
+        class RegexpReplaceFormatter : ISpeficiedFormatter
+        {
+            public string Format(System.Linq.Expressions.MethodCallExpression node)
+            {
+                var innnerTranslator = new BigQueryTranslateVisitor(0, 0);
+
+                var expr1 = innnerTranslator.VisitAndClearBuffer(node.Arguments[0]);
+                var expr2 = innnerTranslator.VisitAndClearBuffer(node.Arguments[1]);
+                var expr3 = innnerTranslator.VisitAndClearBuffer(node.Arguments[2]);
+
+                var format = string.Format("REGEXP_REPLACE({0}, r{1}, {2})", expr1, expr2, expr3);
 
                 return format;
             }
