d57836d54107d39aa283cb8abab75c7e6a539646
neuecc <ils@neue.cc>
2014-09-27 19:48:51 +0900

    add unaded files

---
+++ BigQuery.Linq.Tests/SampleTables/geolitecity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigQuery.Linq.Tests
+{
+    [TableName("[fh-bigquery:geocode.geolite_city_bq_b2b]")]
+    public class geolite_city_bq_b2b
+    {
+        public long? startIpNum { get; set; }
+        public long? endIpNum { get; set; }
+        public long? locId { get; set; }
+        public string country { get; set; }
+        public string region { get; set; }
+        public string city { get; set; }
+        public string postalCode { get; set; }
+        public string latitude { get; set; }
+        public string longitude { get; set; }
+        public string metroCode { get; set; }
+        public string areaCode { get; set; }
+        public long? classA { get; set; }
+        public string startIp { get; set; }
+        public string endIp { get; set; }
+        public string countryLabel { get; set; }
+        public long? classB { get; set; }
+    }
+}
