c065af5aed5ec8c2dd12178dd187cb247fe90f90
Kyle Bremner <kyle@streamba.net>
2016-01-08 10:42:44 +0000

    Checks if LINQPad is installed before trying to do any deployment to LINQPad (closes #47)

--- BigQueryDataContextDriver/DevDeploy.bat
+++ BigQueryDataContextDriver/DevDeploy.bat
@@ -1,6 +1,7 @@
 rem  sn -T assmname
 rem external program - C:\Program Files (x86)\LINQPad5\LINQPad.exe
 
+IF NOT EXIST %programdata%\LINQPad GOTO NOLINQPAD
 taskkill /f /im:LINQPad.exe
 
 rem LINQPad 4
@@ -8,3 +9,5 @@ rem xcopy /i/y *.* "%programdata%\LINQPad\Drivers\DataContext\4.0\BigQueryDataCo
 
 rem LINQPad 5
 xcopy /i/y *.* "%LOCALAPPDATA%\LINQPad\Drivers\DataContext\4.6\BigQueryDataContextDriver (bacdf45c453bd173)\"
+
+:NOLINQPAD
