a907f9b5b671d99827602b6b708f560f1e69149b
Kyle Bremner <kyle@streamba.net>
2016-01-18 13:36:23 +0000

    Re-add test to check converting local DateTime to UTC

--- BigQuery.Linq.Tests/Builder/StandardQueries.cs
+++ BigQuery.Linq.Tests/Builder/StandardQueries.cs
@@ -505,6 +505,15 @@ SELECT
         [TestMethod]
         public void DateTime()
         {
+            var local = new DateTime(2014, 10, 16, 21, 0, 0, DateTimeKind.Local);
+            var offset = TimeZone.CurrentTimeZone.GetUtcOffset(local);
+            var offsetLocal = new DateTime(local.Ticks + offset.Ticks, DateTimeKind.Local);
+            new BigQueryContext().Select(() => new { dt = offsetLocal })
+                            .ToString()
+                            .Is(@"
+SELECT
+  '2014-10-16 21:00:00.000000' AS [dt]".TrimSmart());
+
             new BigQueryContext().Select(() => new { dt = new DateTime(2014, 10, 16, 21, 0, 0, DateTimeKind.Utc) })
                 .ToString()
                 .Is(@"
